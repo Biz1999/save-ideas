0a55b077dfb267687293ea04768a7dd07a9459cd
Lucas Oliveira <lucassioliveira098@gmail.com>
2021-10-20 00:17:40 -0300

    feat(app): created private route

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
 
-import { supabase } from "./assets/apis/supabaseClient";
 import { AuthProvider } from "./contexts/Auth";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import SignUp from "./pages/SingUp";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 function App() {
-  const [session, setSession] = useState(null);
-
-  useEffect(() => {
-    setSession(supabase.auth.session());
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-  }, []);
-
   return (
     <div className="App">
       <Router>
         <AuthProvider>
           <Switch>
-            <Route exact path="/" component={Dashboard} />
+            <PrivateRoute exact path="/" component={Dashboard} />
             <Route path="/signup" component={SignUp} />
             <Route path="/login" component={Login} />
           </Switch>
