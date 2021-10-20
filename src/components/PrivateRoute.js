8f7954cb781b6eb4d34074f5331ae25ce418eaf9
Lucas Oliveira <lucassioliveira098@gmail.com>
2021-10-20 00:17:20 -0300

    fix(app):fixed supabase auth key problems

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
-import { useAuth } from "../../contexts/Auth";
+import { useAuth } from "../contexts/Auth";
 
 export function PrivateRoute({ component: Component, ...rest }) {
   const { user } = useAuth();
@@ -10,8 +10,6 @@ export function PrivateRoute({ component: Component, ...rest }) {
     <Route
       {...rest}
       render={(props) => {
-        // Renders the page only if `user` is present (user is authenticated)
-        // Otherwise, redirect to the login page
         return user ? <Component {...props} /> : <Redirect to="/login" />;
       }}
     ></Route>
